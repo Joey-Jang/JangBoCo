9705e6ca819edd331a07297496156acc899c15d9
종인 <jjonnin.works@gmail.com>
2021-10-27 23:11:39 +0900

    Controller annotation

--- Jangboco/src/main/java/com/gdj37/jangboco/web/diaryjj/controller/DairyjjController.java
+++ Jangboco/src/main/java/com/gdj37/jangboco/web/diaryjj/controller/DairyjjController.java
@@ -10,6 +10,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gdj37.jangboco.web.diaryjj.service.DiaryServiceIF;
 
+@Controller
 public class DairyjjController {
 
 	@Autowired
