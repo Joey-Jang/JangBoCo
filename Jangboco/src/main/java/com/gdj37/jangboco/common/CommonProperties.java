31baea21cab1be2cc565e6c8394e7b741f100cdb
박시원 <devopskick@gmail.com>
2021-11-04 00:29:52 +0900

    Fix Common properties path

--- Jangboco/src/main/java/com/gdj37/jangboco/common/CommonProperties.java
+++ Jangboco/src/main/java/com/gdj37/jangboco/common/CommonProperties.java
@@ -20,8 +20,8 @@ public class CommonProperties {
 	 * 파일 업로드
 	 */
 	//파일 업로드 경로
-	public static final String FILE_UPLOAD_PATH = "D:\\GDJ37\\teamProject\\teamworkspace\\.metadata\\.plugins"
-												+ "\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\Jangboco\\resources\\upload";
+	public static final String FILE_UPLOAD_PATH = "/home/fordevelopers2021/upload";
+
 	
 	//허용파일 확장자
 	public static final String FILE_EXT = "xls|ppt|doc|xlsx|pptx|docx|hwp|csv|jpg|jpeg|png|gif|bmp|tld|txt|pdf|zip|alz|7z";
