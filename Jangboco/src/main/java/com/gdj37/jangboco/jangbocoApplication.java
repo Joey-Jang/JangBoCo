ce63fa686b370893c052d5eb7826bbadcf6d88b7
Joey-Jang <joey.ngn.jang@gmail.com>
2022-03-10 23:58:38 +0900

    migration to SpringBoot in progress

---
+++ Jangboco/src/main/java/com/gdj37/jangboco/jangbocoApplication.java
@@ -0,0 +1,13 @@
+package com.gdj37.jangboco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class jangbocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(jangbocoApplication.class, args);
+    }
+
+}
